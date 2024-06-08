96a33d511ee4500b9b57a085b89d09172fd974aa
danny francisco <dcfran@gmail.com>
2024-06-08 20:27:48 +0800

    add joystick controller widget

--- lib/widgets/joystick_controls.dart
+++ lib/widgets/joystick_controls.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
-import 'joystick_controls.dart';
+import 'package:flutter_joystick/flutter_joystick.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class JoystickControls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Drone Controller',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drone Controller'),
       ),
-      home: JoystickControls(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Throttle/Yaw Control'),
+            SizedBox(
+              height: 150,
+              width: 150,
+              child: Joystick(
+                mode: JoystickMode.all,
+                listener: (details) {
+                  print('x: ${details.x}, y: ${details.y}');
+                },
+              ),
+            ),
+            SizedBox(height: 50),
+            Text('Pitch/Roll Control'),
+            SizedBox(
+              height: 150,
+              width: 150,
+              child: Joystick(
+                mode: JoystickMode.all,
+                listener: (details) {
+                  print('x: ${details.x}, y: ${details.y}');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
